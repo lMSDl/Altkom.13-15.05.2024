94b3a5d4c12007700baca65dfa528ecd5836aa3d
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-13 11:55:42 +0200

    deklaracja, inicjalizacja, przypisywanie wartości

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,25 @@
-﻿
-Console.WriteLine("Hello, Paul!");
+﻿using System;
+
+
+
+Console.WriteLine("Hello, Paul!"); //Console.Write("\n");
+                                   //; - średnik oznacza koniec instrukcji
+
+
+//deklaracja zmiennej lokalnej typu string (ciąg/łańcuch znaków) i nazwie helloVariable
+string helloVariable;
+
+//inicjalizację zmiennej - pierwsze przypisanie wartości
+helloVariable = "Hi";
+//nie możemy używać niezainicjalizowanych zmiennych
+
+Console.WriteLine(helloVariable);
+Console.WriteLine(helloVariable);
+
+//deklaracja z inicjalizacją
+string target = "World";
+Console.WriteLine(target);
+
+//zmiana wartości - każde kolejne przypisanie wartości
+target = Console.ReadLine();
+Console.WriteLine(target);
