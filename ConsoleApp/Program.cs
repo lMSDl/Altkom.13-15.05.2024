15dac77414d5d18b7e5848dfd3a78b912c700cd7
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-14 14:20:07 +0200

    while

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,3 +14,22 @@ using System.Linq;
 //demo.Ifs();
 //demo.Switch();
 
+//musimy zainicjalizować zmienną wartością false, żeby pętla się wykonała chociaż raz
+bool success = false;
+
+//musimy zainicjalizować zmienną, ponieważ istnieje szansza, że pętla się nie wykona i zmienna nie zostanie zainicjalozowana, a za pętlą ma być użyta
+float floatValue = default; //default - inicjalizuje wartością domyślną dla danego typu (dla float to 0)
+
+//while - pętla która trwa gdy jej parametr jest true, parametr jest sprawdzany PRZED każdym wejściem do ciała pętli
+// jeśli przy pierwszym wejściu parametr będzie false, to pętla się nigdy nie wykona
+//while(true) = pętla nieskończona
+while (!success) 
+{
+    Console.WriteLine("Podaj liczbę:");
+    string stringValue = Console.ReadLine();
+
+    success = float.TryParse(stringValue, out floatValue);
+}
+
+
+Console.WriteLine($"Twoja liczba to {floatValue}");
