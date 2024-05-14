fd3e40a9fea5c443f16131f59dc6a0e4116a695a
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-14 11:53:00 +0200

    TryParse (parametr wyjściowy)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,57 +15,69 @@ using System.Linq;
 Console.WriteLine("Podaj bok kwadratu:");
 string input = Console.ReadLine();
 
-int side = int.Parse(input);
+int side; //deklaracja zminnej przekazywanej jako parametr wyjściowy
 
-//if sprawdza warunek w nawiasie i jeśli jest on prawdziwy (true), to wykonuje się blok kodu pod nim
-if (side > 0)
+//metoda TryParse zwraca rezultat mówiący czy parsowanie się powiodło
+//wynik parsowania zwracany jest przez dodatkowy parametr wyjściowy
+//parametry wyjściowe muszą być oznaczone słowem kluczonym "out" zarówno w definicji metody jak i podczas jej używania 
+bool parseSuccess = int.TryParse(input, out side);
+
+if (!parseSuccess)
 {
-    Console.WriteLine($"Kwadrat ma obwód: {side*4}");
+    Console.WriteLine("Błędna wartość");
 }
-// jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
-// else if - może występować wielokrotne
-else if (side < 0)
-{
-    Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
-}
-//else - wykonuje blok kodu w każdym innym przypadku
 else
 {
-    Console.WriteLine("Kwadrat nie istnieje");
-}
+    //if sprawdza warunek w nawiasie i jeśli jest on prawdziwy (true), to wykonuje się blok kodu pod nim
+    if (side > 0)
+    {
+        Console.WriteLine($"Kwadrat ma obwód: {side * 4}");
+    }
+    // jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
+    // else if - może występować wielokrotne
+    else if (side < 0)
+    {
+        Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
+    }
+    //else - wykonuje blok kodu w każdym innym przypadku
+    else
+    {
+        Console.WriteLine("Kwadrat nie istnieje");
+    }
 
 
-//jeżeli używany else jako łącznik ifów, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
-//jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
-//wniosek else jest też łącznikiem między kolejnymi if'ami
+    //jeżeli używany else jako łącznik ifów, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
+    //jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
+    //wniosek else jest też łącznikiem między kolejnymi if'ami
 
-if (side != 0)
-{
-    Console.WriteLine("Bok jest różny od 0");
-}
-if (side == 0)
-{
-    Console.WriteLine("Bok jest równy 0");
-}
+    if (side != 0)
+    {
+        Console.WriteLine("Bok jest różny od 0");
+    }
+    if (side == 0)
+    {
+        Console.WriteLine("Bok jest równy 0");
+    }
 
 
-bool result = side == 0; //== - porównanie
-     result = side != 0; //!= - nierówność
-     result = side > 0; //> - większe
-     result = side < 0;//< - mniejsze
-     result = side >= 0;//>= - większe bądź równe
-     result = side <= 0;//<= - mniejsze bądź równe
+    bool result = side == 0; //== - porównanie
+    result = side != 0; //!= - nierówność
+    result = side > 0; //> - większe
+    result = side < 0;//< - mniejsze
+    result = side >= 0;//>= - większe bądź równe
+    result = side <= 0;//<= - mniejsze bądź równe
 
-     result = side > 0 || side < 0; // || - logiczne lub (OR)
-     result = side > 0 && side < 10; // && - logiczne i (AND)
+    result = side > 0 || side < 0; // || - logiczne lub (OR)
+    result = side > 0 && side < 10; // && - logiczne i (AND)
 
-if(side > 0 && side <= 10)
-{
-    Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
-}
+    if (side > 0 && side <= 10)
+    {
+        Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
+    }
 
-//! - negacja - zaprzeczenie tego co występuje po wykrzykniku
-if (!(side >= 5 && side < 8))
-{
-    Console.WriteLine("Bok kwadratu jest spoza przedziału <5;8)");
+    //! - negacja - zaprzeczenie tego co występuje po wykrzykniku
+    if (!(side >= 5 && side < 8))
+    {
+        Console.WriteLine("Bok kwadratu jest spoza przedziału <5;8)");
+    }
 }
