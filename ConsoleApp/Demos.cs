bb9ef9016c91fc149a240820ddfb2facfac579b8
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-14 13:39:18 +0200

    switch

--- ConsoleApp/Demos.cs
+++ ConsoleApp/Demos.cs
@@ -383,5 +383,78 @@ namespace ConsoleApp
                 Console.WriteLine($"Coś poszło nie tak: {e.Message}");
             }
         }
+
+        public void Ifs()
+        {
+            Console.WriteLine("Podaj bok kwadratu:");
+            string input = Console.ReadLine();
+
+            int side; //deklaracja zminnej przekazywanej jako parametr wyjściowy
+
+            //metoda TryParse zwraca rezultat mówiący czy parsowanie się powiodło
+            //wynik parsowania zwracany jest przez dodatkowy parametr wyjściowy
+            //parametry wyjściowe muszą być oznaczone słowem kluczonym "out" zarówno w definicji metody jak i podczas jej używania 
+            bool parseSuccess = int.TryParse(input, out side);
+
+            if (!parseSuccess)
+            {
+                Console.WriteLine("Błędna wartość");
+            }
+            else
+            {
+                //if sprawdza warunek w nawiasie i jeśli jest on prawdziwy (true), to wykonuje się blok kodu pod nim
+                if (side > 0)
+                {
+                    Console.WriteLine($"Kwadrat ma obwód: {side * 4}");
+                }
+                // jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
+                // else if - może występować wielokrotne
+                else if (side < 0)
+                {
+                    Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
+                }
+                //else - wykonuje blok kodu w każdym innym przypadku
+                else
+                {
+                    Console.WriteLine("Kwadrat nie istnieje");
+                }
+
+
+                //jeżeli używany else jako łącznik ifów, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
+                //jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
+                //wniosek else jest też łącznikiem między kolejnymi if'ami
+
+                if (side != 0)
+                {
+                    Console.WriteLine("Bok jest różny od 0");
+                }
+                if (side == 0)
+                {
+                    Console.WriteLine("Bok jest równy 0");
+                }
+
+
+                bool result = side == 0; //== - porównanie
+                result = side != 0; //!= - nierówność
+                result = side > 0; //> - większe
+                result = side < 0;//< - mniejsze
+                result = side >= 0;//>= - większe bądź równe
+                result = side <= 0;//<= - mniejsze bądź równe
+
+                result = side > 0 || side < 0; // || - logiczne lub (OR)
+                result = side > 0 && side < 10; // && - logiczne i (AND)
+
+                if (side > 0 && side <= 10)
+                {
+                    Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
+                }
+
+                //! - negacja - zaprzeczenie tego co występuje po wykrzykniku
+                if (!(side >= 5 && side < 8))
+                {
+                    Console.WriteLine("Bok kwadratu jest spoza przedziału <5;8)");
+                }
+            }
+        }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApp;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 
 //Demos demo = new Demos();
@@ -10,16 +11,12 @@ using System.Linq;
 //demo.Arrays();
 //demo.Lists();
 //demo.TryCatch();
+//demo.Ifs();
 
 
 Console.WriteLine("Podaj bok kwadratu:");
 string input = Console.ReadLine();
-
-int side; //deklaracja zminnej przekazywanej jako parametr wyjściowy
-
-//metoda TryParse zwraca rezultat mówiący czy parsowanie się powiodło
-//wynik parsowania zwracany jest przez dodatkowy parametr wyjściowy
-//parametry wyjściowe muszą być oznaczone słowem kluczonym "out" zarówno w definicji metody jak i podczas jej używania 
+int side;
 bool parseSuccess = int.TryParse(input, out side);
 
 if (!parseSuccess)
@@ -28,56 +25,22 @@ if (!parseSuccess)
 }
 else
 {
-    //if sprawdza warunek w nawiasie i jeśli jest on prawdziwy (true), to wykonuje się blok kodu pod nim
-    if (side > 0)
+    //switch - przyjmuje parametr, kóry jest porównywany z listą przypadków (case)
+    switch (side)
     {
-        Console.WriteLine($"Kwadrat ma obwód: {side * 4}");
-    }
-    // jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
-    // else if - może występować wielokrotne
-    else if (side < 0)
-    {
-        Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
-    }
-    //else - wykonuje blok kodu w każdym innym przypadku
-    else
-    {
-        Console.WriteLine("Kwadrat nie istnieje");
-    }
-
-
-    //jeżeli używany else jako łącznik ifów, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
-    //jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
-    //wniosek else jest też łącznikiem między kolejnymi if'ami
-
-    if (side != 0)
-    {
-        Console.WriteLine("Bok jest różny od 0");
-    }
-    if (side == 0)
-    {
-        Console.WriteLine("Bok jest równy 0");
-    }
-
-
-    bool result = side == 0; //== - porównanie
-    result = side != 0; //!= - nierówność
-    result = side > 0; //> - większe
-    result = side < 0;//< - mniejsze
-    result = side >= 0;//>= - większe bądź równe
-    result = side <= 0;//<= - mniejsze bądź równe
-
-    result = side > 0 || side < 0; // || - logiczne lub (OR)
-    result = side > 0 && side < 10; // && - logiczne i (AND)
-
-    if (side > 0 && side <= 10)
-    {
-        Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
-    }
-
-    //! - negacja - zaprzeczenie tego co występuje po wykrzykniku
-    if (!(side >= 5 && side < 8))
-    {
-        Console.WriteLine("Bok kwadratu jest spoza przedziału <5;8)");
+        //case - rozpatrywany przypadek
+        //wiele case'ow może być przypisanych do tego samego kodu
+        case > 0:
+            //kod wykonywany jest od case do break - nie ma potrzeby stosowaniea klamerek {}
+            //case musi kończyć się instrukją break - przerywająca wykonywanie swticha
+            Console.WriteLine($"Kwadrat ma obwód: {side * 4}");
+            break;
+        case < 0:
+            Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
+            break;
+        //default - odpowiedniek else, czyli wykonanie kodu, jeśli nie znalezioni odpowiedniego case
+        default:
+            Console.WriteLine("Kwadrat nie istnieje");
+            break;
     }
 }
