d289e591e22b961cd6a82cabdc0e1162e37fdea6
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-14 10:34:35 +0200

    Listy

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApp;
 using System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using System.Linq;
 
 //Demos demo = new Demos();
 //demo.Strings();
@@ -8,4 +9,42 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 //demo.ShowTime(11940);
 //demo.Arrays();
 
+//tworzymy nową listę. Lista po inicjalizacji jest pusta.
+List<string> strings = new List<string>();
 
+Console.WriteLine(strings.Count);
+
+
+//dodajemy nowy element do listy, rozmiar tablicy się zwiększa
+strings.Add("!");
+strings.Add("ala");
+strings.Add("kota");
+
+Console.WriteLine(strings.Count);
+
+//wstawiamy element na konkretny indeks listy - pozostałe ementy przesuwają się
+strings.Insert(2, "ma");
+
+Console.WriteLine(strings.Count);
+
+strings.Add("!");
+strings.Add("!");
+Console.WriteLine(strings.Count);
+
+//usuwamy element pod ostatnim indeksem - rozmiar listy się zmniejsza
+strings.RemoveAt(strings.Count - 1);
+
+//usuwamy element wg wartości - jeśli występuję więcej takich elementów, to usuwany jest pierwszy w kolejności
+strings.Remove("!");
+
+Console.WriteLine(strings.Count);
+
+//w listach odwołujemy się do elementów po indeksach (tak jak w tablicach)
+strings[strings.Count - 1] = "?";
+
+Console.WriteLine(strings[1]);
+
+//przekształcenie tablicy do listy
+strings = Console.ReadLine().Split().ToList();
+
+Console.WriteLine($"Lista ma rozmiar: {strings.Count}");
